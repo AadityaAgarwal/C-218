0beac0f7dd20ae367a6cc51fa08db5e5fbc3ec31
AadityaAgarwal <alka.agarwal31@gmail.com>
2023-09-13 18:20:17 +0530

    new commit

--- public/script.js
+++ public/script.js
@@ -7,21 +7,44 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
-const my_video=document.createElement("video");
-my_video.muted=true;
+const my_video = document.createElement("video");
+my_video.muted = true;
 let mystream;
 navigator.mediaDevices.getUserMedia({
-    audio:true,
-    video:true,
+    audio: true,
+    video: true,
 })
-.then((stream)=>{
-    mystream=stream
-    add_video(my_video,stream)
-})
+    .then((stream) => {
 
-function add_video(video,stream){
-    video.srcObject=stream
-    video.addEventListener("loadedmetadata",()=>{
+        mystream = stream
+        add_video(my_video, stream)
+        socket.on("user connected ", (userId) => {
+            connectNewUser(userId, stream)
+        })
+        peer.on("call",(call)=>{
+            call.answer(stream)
+            const ans_video=document.createElement("video");
+            call.on("stream",(vid_stream)=>{
+                add_video(ans_video,vid_stream)
+            })
+        })
+
+    })
+
+function connectNewUser(userId, stream) {
+    const call=peer.call(userId,stream)
+    
+    const video=document.createElement("video");
+    call.on("stream",(user_video)=>{
+        // addVideoStream(video,user_video);
+        add_video(video,user_video)
+
+    })
+}
+
+function add_video(video, stream) {
+    video.srcObject = stream
+    video.addEventListener("loadedmetadata", () => {
         video.play();
         $("#video_stream").append(video)
     })
